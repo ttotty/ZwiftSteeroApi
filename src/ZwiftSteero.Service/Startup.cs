04285770ef39ab20f0136a789e5f0fec52f4a238
ttotty <twdanaher@gmail.com>
2021-05-06 11:34:02 +1000

    wip

--- src/ZwiftSteero.Service/Startup.cs
+++ src/ZwiftSteero.Service/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace ZwiftSteero.Service
@@ -37,8 +31,7 @@ namespace ZwiftSteero.Service
                     options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
                     options.JsonSerializerOptions.WriteIndented = true;
                     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-                });    
-
+                });
         
             services.AddSwaggerGen(c =>
             {
